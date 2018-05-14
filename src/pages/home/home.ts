e87916eeac7da96d57e3cf4bccab24ba33576088
mrizwan <rizwan@servup.work>
2018-05-15 00:56:03 +0500

    new content added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ToastController } from 'ionic-angular';
-import { DirectoryPage } from '../directory/directory';
+
 
 @Component({
   selector: 'page-home',
@@ -9,15 +8,6 @@ import { DirectoryPage } from '../directory/directory';
 })
 export class HomePage {
   
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController) { }
-  loginUser() {
-    let toast = this.toastCtrl.create({
-      message: 'User login successfully',
-      duration: 3000
-    });
-    toast.present();
-  }
-  register(){
-    this.navCtrl.push(DirectoryPage);
-  }
+  constructor(public navCtrl: NavController) { }
+  
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { ToastController } from 'ionic-angular';
+import { DirectoryPage } from '../directory/directory';
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
    }
+  loginUser() {
+    let toast = this.toastCtrl.create({
+      message: 'User login successfully',
+      duration: 3000
+    });
+    toast.present();
+  }
+  register() {
+    this.navCtrl.push(DirectoryPage);
+  }
 }
